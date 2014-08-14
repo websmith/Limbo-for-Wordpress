702d9af6bcf0e5ed7bddc27cca3bc2108e86ec2e
websmith <nate@holeintheroof.com>
2014-08-14 20:46:20 +0000

    add duplicate parent page link in nav dropdowns

--- limbo/js/site.js
+++ limbo/js/site.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	$('.widget').addClass('well');
+	
+	//Duplicate dropdown page in dropdown
+	$('.nav li.dropdown ul.dropdown-menu').each(function() {
+		var copyThis = $(this).siblings('a');
+		var cloned = copyThis.clone();
+		cloned.prependTo(this).wrap('<li class="menu-item menu-item-object-page"></li>');
+		cloned.removeAttr('class');
+		cloned.removeAttr('data-toggle');
+		cloned.removeAttr('aria-haspopup');
+		cloned.find(".caret").remove();
+	});
 });
